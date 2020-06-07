0a25621ea38343769a0816e12a7feee7743a4150
Newbie-W <1078957527@qq.com>
2020-06-07 17:46:21 +0800

    增加 206. 反转链表 题解

---
+++ C、C++/206. 反转链表/Solution.cpp
@@ -0,0 +1,31 @@
+/*
+执行用时 :16 ms, 在所有 C++ 提交中击败了17.12%的用户
+内存消耗 :8.3 MB, 在所有 C++ 提交中击败了100.00%的用户
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) { //无头结点
+        if (head==NULL)
+            return NULL;
+        ListNode *pre = NULL;
+        ListNode *cur = head;
+        ListNode *next = cur->next;
+        while (cur->next!=NULL) {
+            //printf("~ cur = %d\n", cur->val);
+            next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        cur->next = pre;
+        return cur;
+    }
+};
